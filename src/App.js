484cc38a2f16032417ce829166f1a8cc51994070
Exo Sol <1.petarzlatar@gmail.com>
2020-11-21 19:20:52 +0100

    first stop stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  const [play, setPlay] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    if (play) {
+      const id = window.setInterval(() => {
+        setSeconds((sec) => sec + 1);
+      }, 1000);
+      setIntervalId(id);
+    } else {
+      window.clearInterval(intervalId);
+    }
+  }, [play]);
+
+  const secConverter = (sec) => {
+    let sekunde, minute, sati;
+
+    if (sec >= 3600) {
+      sekunde = sec % 60;
+      sati = Math.floor(sec / 3600);
+      minute = Math.floor(sec / 60) - sati * 60;
+
+      return `${sati} : ${minute} : ${sekunde}`;
+    } else if (sec >= 60) {
+      sekunde = sec % 60;
+      minute = Math.floor(sec / 60);
+
+      return `${minute} : ${sekunde}`;
+    } else {
+      return ` ${sec}`;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{secConverter(seconds)}</h1>
+
+      <span
+        className="material-icons"
+        id="play"
+        onClick={() => {
+          play === true ? setPlay(false) : setPlay(true);
+          console.log(play);
+          console.log("clicked");
+        }}
+        style={
+          !play
+            ? { color: "green", boxShadow: "0px 0px 50px 2px rgb(34, 116, 14)" }
+            : { color: "red", boxShadow: "0px 0px 50px 2px red" }
+        }
+      >
+        {!play ? "play_circle_outline" : "stop"}
+      </span>
     </div>
   );
 }
