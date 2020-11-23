e6b0810cd36f5bf70ed5222305b66f90fef3df73
Exo Sol <1.petarzlatar@gmail.com>
2020-11-23 18:51:14 +0100

    finalized, nedd to test for bugs and maybe a bit of design

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import SaveComp from "./SaveComp.js";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 
 function App() {
   const [seconds, setSeconds] = useState(0);
   const [play, setPlay] = useState(false);
   const [intervalId, setIntervalId] = useState(null);
   const [timeStamp, setTimeStamp] = useState([]);
+  const [totTime, setTotTime] = useState(0);
 
   // COUNTER /////////////////////////////////////
   useEffect(() => {
     if (play) {
       const id = window.setInterval(() => {
         setSeconds((sec) => sec + 1);
-      }, 1000);
+      }, 100);
       setIntervalId(id);
     } else {
       window.clearInterval(intervalId);
@@ -48,75 +50,128 @@ function App() {
     console.log(timeStamp);
   };
 
-  // SAVIN SELECTED TIME(timestamps) TO LOCAL STORAGE
-  const saveToLocal = (e) => {
-    //Unpacking
+  ///// Unpacking minutes for saving
+
+  const unPack = (e) => {
     let time = e.target.innerText;
     let timeS = time.split(":");
     console.log(timeS);
     if (timeS.length > 2) {
-      let sec =
-        parseInt(timeS[0]) * 3600 +
-        parseInt(timeS[1]) * 60 +
-        parseInt(timeS[2]);
-      console.log(sec);
+      console.log("prva");
+      return (
+        parseInt(timeS[0]) * 3600 + parseInt(timeS[1]) * 60 + parseInt(timeS[2])
+      );
     } else if (timeS.length > 1) {
-      let sec = parseInt(timeS[0]) * 60 + parseInt(timeS[1]);
-      console.log(sec);
+      console.log("druga");
+      return parseInt(timeS[0]) * 60 + parseInt(timeS[1]);
     } else {
-      console.log(timeS);
+      console.log("treca");
+      return parseInt(timeS);
     }
+  };
+
+  // SAVIN SELECTED TIME(timestamps) TO LOCAL STORAGE
+  const saveToLocal = (e) => {
+    let timeStampInSeconds = unPack(e);
+    console.log(timeStampInSeconds);
+    // initial setup
+    if (localStorage.getItem("totalTime") === null) {
+      localStorage.setItem("totalTime", String(timeStampInSeconds));
+    } else {
+      let time = parseInt(localStorage.getItem("totalTime"));
+      time += timeStampInSeconds;
+      localStorage.setItem("totalTime", String(time));
+    }
+  };
+
+  //RETRIVE FROM LOCAL AND RENDER IN Format hours
+  const totalAndRender = () => {
+    let time = parseInt(localStorage.getItem("totalTime"));
+    return secConverter(time);
   };
 
   /////////////////////////////////////////////////////
 
   return (
-    <div className="App">
-      <h1>{secConverter(seconds)}</h1>
-      <div className="buttons">
-        <span
-          className="material-icons"
-          id="play"
-          onClick={() => {
-            play === true ? setPlay(false) : setPlay(true);
-            console.log(play);
-            console.log("clicked");
-          }}
-          style={
-            !play
-              ? {
-                  color: "rgb(44, 233, 76)",
-                  boxShadow: "0px 0px 30px 2px rgb(44, 233, 76)",
-                }
-              : { color: "red", boxShadow: "0px 0px 30px 2px red" }
-          }
-        >
-          {!play ? "play_arrow" : "stop"}
-        </span>
-        <SaveComp sec={seconds} passFunc={timeGrabber} />
-        <span
-          id="delete"
-          className="material-icons"
-          onClick={() => {
-            setSeconds(0);
-            setTimeStamp([]);
-            setPlay(false);
-          }}
-        >
-          remove_circle
-        </span>
-        <span className="material-icons" id="folder">
-          folder
-        </span>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Fragment>
+                <h1>{secConverter(seconds)}</h1>
+                <div className="buttons">
+                  <span
+                    className="material-icons"
+                    id="play"
+                    onClick={() => {
+                      play === true ? setPlay(false) : setPlay(true);
+                      console.log(play);
+                      console.log("clicked");
+                    }}
+                    style={
+                      !play
+                        ? {
+                            color: "rgb(44, 233, 76)",
+                            boxShadow: "0px 0px 30px 2px rgb(44, 233, 76)",
+                          }
+                        : { color: "red", boxShadow: "0px 0px 30px 2px red" }
+                    }
+                  >
+                    {!play ? "play_arrow" : "stop"}
+                  </span>
+                  <SaveComp sec={seconds} passFunc={timeGrabber} />
+                  <span
+                    id="delete"
+                    className="material-icons"
+                    onClick={() => {
+                      setSeconds(0);
+                      setTimeStamp([]);
+                      setPlay(false);
+                    }}
+                  >
+                    remove_circle
+                  </span>
+                  <Link to="/tot">
+                    <span className="material-icons" id="folder">
+                      folder
+                    </span>
+                  </Link>
+                </div>
+                <div className="timeStamps">
+                  {timeStamp.map((time, index) => (
+                    <h3 key={index} onClick={saveToLocal}>
+                      {time}
+                    </h3>
+                  ))}
+                </div>
+              </Fragment>
+            )}
+          />
+          <Route
+            exact
+            path="/tot"
+            render={(props) => (
+              <Fragment>
+                <h2>Total time</h2>
+                <p id="total">{totalAndRender()}</p>
+                <Link to="/">
+                  <span
+                    id="folder"
+                    className="material-icons"
+                    style={{ marginLeft: "25px" }}
+                  >
+                    arrow_back
+                  </span>
+                </Link>
+              </Fragment>
+            )}
+          />
+        </Switch>
       </div>
-      <div className="timeStamps">
-        {timeStamp.map((time, index) => (
-          <h3 id={index} onClick={saveToLocal}>
-            {time}
-          </h3>
-        ))}
-      </div>
-    </div>
+    </Router>
   );
 }
 
